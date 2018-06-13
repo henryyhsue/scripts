1068c1330e73f8a3403af5dc16a14acc1ec16d9d
Henry Hsue <hhenry@uber.com>
2018-06-12 19:15:22 -0700

    updated

--- daily_timestamps.sh
+++ daily_timestamps.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-printf "\n\n=============================\n$(date)\n=============================\n\n" | tee -a /Users/henryhsue/Work/Notes/NotesDaily /Users/henryhsue/Work/Notes/TasksDaily 1>/dev/null
-cd /Users/henryhsue/Work/Notes/; 
-#rsync -av --exclude=*.swp /Users/henryhsue/Work/Notes/ oldsmobile-arnage-7.dev.uber.com:~/notes
+printf "\n\n=============================\n$(date)\n=============================\n\n" | tee -a ~/work/work-notes/NotesDaily ~/work/work-notes/TasksDaily 1>/dev/null
